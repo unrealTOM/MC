5133946cd68165a50286269dc38987c439c2d7df
unknown <freshthomas@gmail.com>
2018-07-14 13:44:37 +0800

    merge image scripts

---
+++ Combine.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+
+files = [
+  'normal1.png',
+  'normal2.png']
+#  'uniform1.png',
+#  'uniform2.png']
+
+halfWidth = 400
+#halfWidth = 300
+height = 240
+#height = 220
+result = Image.new("RGBA", (halfWidth * 2, height))
+
+for index, file in enumerate(files):
+  path = os.path.expanduser(file)
+  img = Image.open(path)
+  x = index % 2 * halfWidth 
+  y = index // 2 * height
+  w, h = img.size
+  print(x,y,w,h)
+  result.paste(img, (x, height - h, x + w, height))
+
+result.save(os.path.expanduser('normal.png'))
+#result.save(os.path.expanduser('uniform.png'))
